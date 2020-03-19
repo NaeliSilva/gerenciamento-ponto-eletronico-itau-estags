f7bc489ec7722fd7d8c7b45607b244e2b19b03c8
NaeliSilva <naelisilva15@gmail.com>
2020-03-19 13:40:54 -0300

    Iniciado backend

---
+++ backend/src/main/java/com/itau/gerenciamentopontoeletronicoitauestags/GerenciamentoPontoEletronicoItauEstagsApplication.java
@@ -0,0 +1,13 @@
+package com.itau.gerenciamentopontoeletronicoitauestags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoPontoEletronicoItauEstagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoPontoEletronicoItauEstagsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/itau/gerenciamentopontoeletronicoitauestags/GerenciamentoPontoEletronicoItauEstagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.gerenciamentopontoeletronicoitauestags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPontoEletronicoItauEstagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
