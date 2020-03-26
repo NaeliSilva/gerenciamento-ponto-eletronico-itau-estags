66daacea1ba421f58e5d0ef22bced7b70620a261
Naeli <naelisilva15@gmail.com>
2020-03-26 20:42:01 -0300

    frontend iniciado

---
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+
+];
---
+++ frontend/src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { LoginComponent } from '../../pages/login/login.component';
+
+export const AuthLayoutRoutes: Routes = [
+    { path: 'login',          component: LoginComponent },
+];
