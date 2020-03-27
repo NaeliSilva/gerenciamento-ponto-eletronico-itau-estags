05427cfbe9e6f6646f8e4eb2796921eac205636f
NaeliSilva <naelisilva15@gmail.com>
2020-03-27 17:58:54 -0300

    Criação do ponto

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RequestService } from 'src/app/services/request';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+export class LoginComponent implements OnInit {
+  constructor(private requestService: RequestService) {}
+  data = {
+    usuarioId: 0,
+    tipo: null,
+  }
 
   ngOnInit() {
   }
-  ngOnDestroy() {
+
+  async submit() {
+    await this.requestService.send('ponto', 'POST', this.data);
   }
 
 }
